a4c5021ccfa036010ce1fb54ffe2f148c255a53d
jnendef <josemanuel.naveiro@endef.com>
2025-07-02 08:31:06 +0000

    Explicacion de descarga de informacion

--- app/pages/3_Resultados_Generales.py
+++ app/pages/3_Resultados_Generales.py
@@ -21,6 +21,10 @@ st.sidebar.markdown(
 
 st.markdown("# Análisis de Comunidad")
 
+with st.expander("Descarga de la información"):
+    st.write("Este documento permite su descarga en formato PDF haciendo click en los tres puntos de la esquina superior derecha de la página web. Se desplegará un menú donde está la opción de imprimir, que es donde se tendrá la opción de descargarlo en formato PDF.")
+    st.write("También es posible la descarga de los gráficos y las tablas pasando el ratón por la esquina superior derecha del gráfico o de la tabla. En el caso de las tablas aparecerán las opciones de maximizar y tres puntitos que al clickar mostrarán las opciones, entre las que se encuentran el guardar la imagen en formato png. En las tablas al pasar el ratón aparecerán en la esquina superior derecha tres opciones. La primera de esas opciones permite descargar la información de la tabla en formato csv.")
+
 meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
 st.write("Zaragoza, "+str(dt.datetime.today().day)+" de "+meses[dt.datetime.today().month-1]+" de "+str(dt.datetime.today().year))
